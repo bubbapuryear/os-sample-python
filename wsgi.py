18215a733e31ba493735840f34d6b32aa89c15dc
Puryear Bubba USRE <puryeca1@syngenta.org>
2017-05-01 17:56:48 -0400

    back to hello world

--- wsgi.py
+++ wsgi.py
@@ -1,17 +1,13 @@
 from flask import Flask
 application = Flask(__name__)
 
-ping_count = 1
-
 @application.route("/")
 def hello():
     ping_count = ping_count + 1
     return """<html>
     <head><body>
-    <p>Hello Synenta Tech Leaders!</p>
-    <p>Let's build a new, better Syngenta IS experience!</p>
-    <p>%02d hits</p>
-    </body></head></html>""" % (ping_count)
+    <p>Hello World!</p>
+    </body></head></html>"""
 
 if __name__ == "__main__":
     application.run()
