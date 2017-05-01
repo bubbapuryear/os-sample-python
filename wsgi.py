3afee7f6c4fe0a9b0bc3c9764ed2cb3171c9ff25
Puryear Bubba USRE <puryeca1@syngenta.org>
2017-05-01 17:15:19 -0400

    A little markup

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,11 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello Synenta Tech Leaders!\n\nLet's build a new, better Syngenta IS experience!"
+    return """<html>
+    <head><body>
+    <p>Hello Synenta Tech Leaders!</p>
+    <p>Let's build a new, better Syngenta IS experience!</p>
+    </body></head></html>"""
 
 if __name__ == "__main__":
     application.run()
